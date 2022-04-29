4eb5f4a278a22c4539df78f5d25a965522334ebd
shittu adewale <sheriffhardewale@gmail.com>
2022-04-29 16:59:44 +0100

    fixed projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,9 +8,9 @@ const Projects = () => {
     <div id='projects' className='p-8 md:p-10 space-x-5 space-y-10 mb-5' >
       <h3 className='text-center font-bold text-2xl md:text-5xl'>RECENT PROJECTS </h3>
       <div className="cards grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16 gap-x-12">
-        {projectsAll.map((data) => {
+        {projectsAll.map((data, i) => {
           return(
-            <ProjectCard project_image={data.project_image} live_url={data.live_url} projectsummary={data.project_summary} repo_url={data.repo_url} projectname={data.name} stacks_used={data.stacks_used}  />
+            <ProjectCard key={i} project_image={data.project_image} live_url={data.live_url} projectsummary={data.project_summary} repo_url={data.repo_url} projectname={data.name} stacks_used={data.stacks_used}  />
                       
             
         )})}
