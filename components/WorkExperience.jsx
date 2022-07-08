dfee1cab9f1559750991fda042d09016e762567a
shittu adewale <sheriffhardewale@gmail.com>
2022-07-08 23:20:55 +0100

    work experience

--- components/WorkExperience.jsx
+++ components/WorkExperience.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const WorkExperience = () => {
   return (
     <div className='flex items-center animate-bounce justify-center text-2xl' id='work-experience' >
+       <h1 className='text-center' >Work Experince </h1>
         coming soon....
     </div>
   )
