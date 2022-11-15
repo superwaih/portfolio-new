676086081cc0d37241c61ecfd8042e42b54fbf32
shittu adewale <sheriffhardewale@gmail.com>
2022-11-16 00:50:13 +0100

    adjusted card height and width

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -6,10 +6,10 @@ import {GoLinkExternal} from "react-icons/go"
 const ProjectCard = ({project_image, projectname, projectsummary, stacks_used, live_url, repo_url}) => {
   return (
     
-    <div className="project-card gap-4 h-[630px] bg-black text-white  card items-center flex flex-col w-full    rounded-[1.2rem] transform hover:scale-105 duration-500 shadow dark:bg-DarkModeElement">
-    <div className="card-img card-img object-contain  w-full  h-2/5">
+    <div className="project-card gap-4 h-[630px] bg-black text-white  card items-center flex flex-col w-full max-w-[400px]    rounded-[1.2rem] transform hover:scale-105 duration-500 shadow dark:bg-DarkModeElement">
+    <div className="card-img card-img object-contain  w-full h-full">
      
-      <img className='rounded-t-md w-full h-full object-cover' src={project_image} alt="project image" />
+      <img className='rounded-t-md w-full h-[300px] object-cover' src={project_image} alt="project image" />
     </div>
     <div className="project-details flex flex-col justify-between p-5 h-full w-full ">
       <h4 className='font-bold' >{projectname}</h4>
@@ -23,7 +23,7 @@ const ProjectCard = ({project_image, projectname, projectsummary, stacks_used, l
               )
           })}
       </div>
-      <div className="button flex font-bold justify-between">
+      <div className="button flex font-bold gap-2 justify-between">
         <a target="_blank" rel="noreferrer" className='flex gap-4 items-center border p-2' href={live_url}>Live Site <GoLinkExternal /></a>
         <a target="_blank" rel="noreferrer" className='border p-2 flex gap-4 items-center' href={repo_url}>Source code  <GoLinkExternal /></a>
       </div>
